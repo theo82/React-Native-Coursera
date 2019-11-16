230064e02b90c6b47831b20c58c3c92e0b25ebfd
Theodosios Tziomakas <theoaristi53@gmail.com>
2019-11-16 08:22:27 +0200

    first commit

---
+++ ios/build/confusion/Build/Intermediates.noindex/confusion.build/Debug-iphonesimulator/confusion.build/DerivedSources/confusion_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char confusionVersionString[];
+ extern const double confusionVersionNumber;
+
+ const unsigned char confusionVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:confusion  PROJECT:confusion-1" "\n";
+ const double confusionVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ ios/build/confusion/Build/Intermediates.noindex/confusion.build/Debug-iphonesimulator/confusion.build/Objects-normal/x86_64/confusion_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/theodosiostziomakas/Desktop/React-Native/confusion/ios/build/confusion/Build/Intermediates.noindex/confusion.build/Debug-iphonesimulator/confusion.build/DerivedSources/confusion_vers.c
