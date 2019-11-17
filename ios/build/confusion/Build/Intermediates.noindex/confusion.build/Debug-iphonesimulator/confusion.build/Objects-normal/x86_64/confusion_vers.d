582cc3632a44a4e1b5839c833847e3036effbb7d
Theodosios Tziomakas <theoaristi53@gmail.com>
2019-11-17 14:24:48 +0200

    Navigation Part 1

--- ios/build/confusion/Build/Intermediates.noindex/confusion.build/Debug-iphonesimulator/confusion.build/Objects-normal/x86_64/confusion_vers.d
+++ ios/build/confusion/Build/Intermediates.noindex/confusion.build/Debug-iphonesimulator/confusion.build/Objects-normal/x86_64/confusion_vers.d
@@ -1,2 +1,2 @@
 dependencies: \
-  /Users/theodosiostziomakas/Desktop/React-Native/confusion/ios/build/confusion/Build/Intermediates.noindex/confusion.build/Debug-iphonesimulator/confusion.build/DerivedSources/confusion_vers.c
+  /Users/theodosiostziomakas/Desktop/React-Native-Coursera/ios/build/confusion/Build/Intermediates.noindex/confusion.build/Debug-iphonesimulator/confusion.build/DerivedSources/confusion_vers.c
